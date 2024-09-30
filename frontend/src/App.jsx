27ba3c561bfd7323244b9346421e4a0cd27be4b0
franciscogallom <franciscogallomolinuevo@gmail.com>
2024-09-29 23:53:33 -0300

    Create frontend project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <h1>números generados 🔒📛💻</h1>
+      <div className="card">
+        <h4>✔️ 1234214234234</h4>
+        <h4>✔️ 5678426245325</h4>
+        <h4>✔️ 3456662346576</h4>
+        <h4>✔️ 1234214234234</h4>
+        <h4>✔️ 3456662346576</h4>
+        <h4>...</h4>
+      </div>
+    </>
+  )
+}
+
+export default App
