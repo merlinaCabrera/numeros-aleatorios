abb30bb5947bc4f7995e2b4ff9bad264e65c2652
franciscogallom <franciscogallomolinuevo@gmail.com>
2024-10-22 18:18:39 -0300

    Fetch data from server

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,34 @@
+import { useEffect, useState } from "react"
 import "./App.css"
 
 function App() {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/data")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data)
+        setLoading(false)
+      })
+      .catch((error) => {
+        console.error("Error al obtener la data:", error)
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) return <div className="spinner"></div>
+
   return (
     <>
       <h1>números generados 🔒📛💻</h1>
       <div className="card">
-        <h4>✔️ 1234214234234</h4>
-        <h4>✔️ 5678426245325</h4>
-        <h4>✔️ 1456462546576</h4>
-        <h4>✔️ 7236232348734</h4>
-        <h4>✔️ 6674623465196</h4>
+        {data.map((item, index) => (
+          <h4 key={index}>
+            ✔️ {item.number} [ seed: {item.seed} ]
+          </h4>
+        ))}
         <h4>...</h4>
       </div>
     </>
