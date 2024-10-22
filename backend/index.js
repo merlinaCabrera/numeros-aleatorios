1e4ee511767efd7646c862b21677c94a531cc836
franciscogallom <franciscogallomolinuevo@gmail.com>
2024-10-22 17:08:47 -0300

    Add basic POST method

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import { pool } from "./mysql.js"
 const app = express()
 const port = 3000
+app.use(express.json())
 
 app.get("/health-check", (req, res) => {
   res.send(`Server running on port ${port}!`)
@@ -10,6 +11,19 @@ app.get("/health-check", (req, res) => {
 app.get("/api/data", async (req, res) => {
   const [result] = await pool.query("SELECT * FROM generated_numbers")
   res.json(result)
+})
+
+app.post("/api/data", async (req, res) => {
+  const { number, seed } = req.body
+  const [rows] = await pool.query("INSERT INTO generated_numbers(number, seed) VALUES (?, ?)", [
+    number,
+    seed,
+  ])
+  res.send({
+    id: rows.insertId,
+    number,
+    seed,
+  })
 })
 
 app.listen(port, () => {
