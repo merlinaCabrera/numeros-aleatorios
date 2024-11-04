075750f10ea13d6fa07f8e5298a461b51ddbf8a8
franciscogallom <franciscogallomolinuevo@gmail.com>
2024-11-04 20:00:22 -0300

    Add port to MySQL connection

--- backend/mysql.js
+++ backend/mysql.js
@@ -1,9 +1,10 @@
 import { createPool } from "mysql2/promise.js"
-import { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } from "./config.js"
+import { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } from "./config.js"
 
 export const pool = createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_DATABASE,
+  port: DB_PORT,
 })
